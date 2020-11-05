680b751b4a40b75dc80df7142371fc991e4d0661
Fin <finlaytgeorge@gmail.com>
2020-11-05 16:10:27 +1100

    add show listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,16 @@
 class ListingsController < ApplicationController
+    before_action :set_listing, only: [:show, :destroy]
+
     def index
         @listings = Listing.all
     end
+
+    def show
+    end
+
+    private
+
+    def set_listing
+        @listing = Listing.find(params[:id])
+      end
 end
