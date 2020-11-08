55ac6021bef95073ab209d776f589309923bbc33
Fin <finlaytgeorge@gmail.com>
2020-11-09 09:33:07 +1100

    edit/update added and working on account

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,7 @@
 class ListingsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     before_action :listing_params, only: [:create]
-    before_action :set_listing, only: [:show]
+    before_action :set_listing, only: [:show, :edit, :update, :destroy]
 
     def index
         @listings = Listing.all
@@ -45,8 +45,18 @@ class ListingsController < ApplicationController
         redirect_to listings_path, notice: "Listing was successfully created"
     end
 
+    def edit
+    end
+
+    def update
+        if @listing.update(listing_params)
+            redirect_to @listing
+        else
+            render "edit"
+        end
+    end
+
     def destroy
-        @listing = Listing.find(params[:id])
         if @listing.present?
           @listing.destroy
         end
