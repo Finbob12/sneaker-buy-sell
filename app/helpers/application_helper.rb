e09e15925de79ff1782526494b4a5d09b0a08d13
Fin <finlaytgeorge@gmail.com>
2020-11-10 14:18:10 +1100

    fix logged out show, working on messages helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,29 +5,10 @@ module ApplicationHelper
         unread_messages = Message.where("user_id != ? AND read = ?", current_user.id, false).where(conversation_id: [conversations]).count
      
         if unread_messages == 0
-            return link_to "Messages", conversations_path, class: "btn btn-warning"
+            return link_to "Messages", conversations_path, class: "nav-link bg-danger"
         else
-           return link_to "Messages", conversations_path
+           return link_to "Messages", conversations_path, class: "nav-link"
         end
 
       end
-
-    def get_chat_users(conv)
-        if conv.sender == current_user
-            link_to conv.recipient.username, conversation_messages_path(conv), class: "btn btn-warning"
-        elsif conv.sender != current_user
-            link_to conv.sender.username, conversation_messages_path(conv), class: "btn btn-warning"
-        else
-            return
-        end
-    end
-
-    def get_message_notifications(conv)
-        if conv.messages.last.user != current_user
-            "received new message #{time_ago_in_words(conv.messages.last.updated_at)} ago from"
-
-        else
-            " you sent a message #{time_ago_in_words(conv.messages.last.updated_at)} ago to"
-        end
-    end
 end
