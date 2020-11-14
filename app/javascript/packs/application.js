b99d2889e34a0eecc35c2ef354d33b053629326e
Fin <finlaytgeorge@gmail.com>
2020-11-14 13:18:42 +1100

    add new form styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import './src/application.scss'
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("custom/form")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
